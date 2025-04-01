da908255b7a059232d756f3cebb6a48d8960b11a
Murilo Alves Melo <murilo.melo72@gmail.com>
2025-03-31 23:34:01 -0300

    add splashView and SplashViewController

--- Remider/Sources/Features/Splash/SplashView.swift
+++ Remider/Sources/Features/Splash/SplashView.swift
@@ -6,3 +6,54 @@
 //
 
 import Foundation
+import UIKit
+
+class SplashView: UIView {
+    
+    private let logoImageView: UIImageView = {
+        let image = UIImageView()
+        image.image = UIImage(named: "Logo")
+        image.contentMode = .scaleAspectFill
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
+    
+    private let titleLogoLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Remind"
+        label.font = UIFont.boldSystemFont(ofSize: 42)
+        label.textColor = .white
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    init(){
+        super.init(frame: .zero)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI(){
+
+        self.addSubview(logoImageView)
+        self.addSubview(titleLogoLabel)
+        
+        setupConstraints()
+    }
+    
+    private func setupConstraints(){
+        NSLayoutConstraint.activate([
+            
+            titleLogoLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor, constant: Metrics.medium),
+            titleLogoLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: -Metrics.medium),
+            
+            logoImageView.trailingAnchor.constraint(equalTo: titleLogoLabel.leadingAnchor, constant: -Metrics.tiny),
+            logoImageView.bottomAnchor.constraint(equalTo: titleLogoLabel.bottomAnchor),
+            
+            
+        ])
+    }
+}
--- Remider/Sources/Features/Splash/SplashViewController.swift
+++ Remider/Sources/Features/Splash/SplashViewController.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+import UIKit
+
+class SplashViewController: UIViewController {
+    let contentView = SplashView()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.isHidden = true
+        self.view.backgroundColor = Colors.redBase
+        setup()
+    }
+    
+    private func setup(){
+        self.view.addSubview(contentView)
+        
+        setupConstraints()
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            contentView.topAnchor.constraint(equalTo: view.topAnchor),
+            contentView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            contentView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
+        
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+    }
+}
