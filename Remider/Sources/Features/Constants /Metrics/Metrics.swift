f2862137463f023623837aaaf778e4a7f30c9591
Murilo Alves Melo <murilo.melo72@gmail.com>
2025-03-31 23:34:19 -0300

    add constants text, colros and metrics

--- Remider/Sources/Features/Constants /Metrics/Metrics.swift
+++ Remider/Sources/Features/Constants /Metrics/Metrics.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+import UIKit
+
+public struct Metrics {
+    public static var huge: CGFloat = 48
+    public static var medium: CGFloat = 24
+    public static var small: CGFloat = 12
+    public static var tiny: CGFloat = 8
+    public static var little: CGFloat = 4
+}
--- Remider/Sources/Features/Constants /Text/Text.swift
+++ Remider/Sources/Features/Constants /Text/Text.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+import UIKit
+
+public struct Typography {
+    static let heading = UIFont.systemFont(ofSize: 20, weight: .heavy)
+    static let subheading = UIFont.systemFont(ofSize: 16, weight: .heavy)
+    static let input = UIFont.systemFont(ofSize: 16, weight: .regular)
+    static let label = UIFont.systemFont(ofSize: 14, weight: .semibold)
+    static let body = UIFont.systemFont(ofSize: 14, weight: .regular)
+    static let tag = UIFont.systemFont(ofSize: 12, weight: .regular)
+}
