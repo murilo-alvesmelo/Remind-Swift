71ff862dd53ca6baed432880f561a55d7c1a45d8
Murilo Alves Melo <murilo.melo72@gmail.com>
2025-04-02 23:38:21 -0300

    🛠️ add localizable for strings  🛠️

---
+++ Remider/Sources/Features/Extensions/String.swift
@@ -0,0 +1,14 @@
+//
+//  String.swift
+//  Remider
+//
+//  Created by Murilo Alves on 02/04/25.
+//
+
+import Foundation
+
+extension String {
+    var localizes: String {
+        return NSLocalizedString(self, comment: "")
+    }
+}
